e8cea5b08cc6a360d3cb18db37e5c0dd81113f80
Holger Arnold <git@harnold.org>
2013-01-01 12:24:30 +0100

    Add xmalloc.c, xmalloc.h

---
+++ src/xmalloc.c
@@ -0,0 +1,20 @@
+#include <errno.h>
+#include <stdlib.h>
+
+#include "error.h"
+#include "xmalloc.h"
+
+void *xmalloc(size_t size)
+{
+    if (size == 0)
+        size = 1;
+
+    void *ptr = malloc(size);
+
+    if (!ptr) {
+        error_exit(
+            errno, "Out of memory?  malloc failed to allocate %ul bytes", size);
+    }
+
+    return ptr;
+}

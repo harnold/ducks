957e398f91a153251334658d7a5242c0cfba9e3f
Holger Arnold <git@harnold.org>
2013-01-08 17:29:36 +0100

    dpmi: Make checks for set carry flag explicit

--- src/dpmi.c
+++ src/dpmi.c
@@ -20,7 +20,7 @@ int dpmi_map_physical_address(uint32_t phys_addr, uint32_t size, uint32_t *lin_a
     regs.w.di = LWORD(size);
     int386(DPMI_INT, &regs, &regs);
 
-    if (regs.x.cflag)
+    if (regs.x.cflag != 0)
         return DPMI_ERROR;
 
     *lin_addr = ((uint32_t) regs.w.bx << 16 ) + regs.w.cx;
@@ -37,7 +37,7 @@ int dpmi_unmap_physical_address(uint32_t lin_addr)
     regs.w.cx = LWORD(lin_addr);
     int386(DPMI_INT, &regs, &regs);
 
-    if (regs.x.cflag)
+    if (regs.x.cflag != 0)
         return DPMI_ERROR;
 
     return DPMI_SUCCESS;
@@ -56,7 +56,7 @@ int dpmi_simulate_rm_interrupt(unsigned int inum, struct dpmi_rm_info *info)
     regs.x.edi = FP_OFF(info);
     int386x(DPMI_INT, &regs, &regs, &sregs);
 
-    if (regs.x.cflag)
+    if (regs.x.cflag != 0)
         return DPMI_ERROR;
 
     return DPMI_SUCCESS;
@@ -71,7 +71,7 @@ int dpmi_allocate_dos_memory(uint32_t size, uint16_t *segment, uint16_t *selecto
     regs.w.bx = (uint16_t) ((size + 15) >> 4);
     int386(DPMI_INT, &regs, &regs);
 
-    if (regs.x.cflag)
+    if (regs.x.cflag != 0)
         return DPMI_ERROR;
 
     *segment  = regs.w.ax;
@@ -89,7 +89,7 @@ int dpmi_free_dos_memory(uint16_t selector)
     regs.w.dx = selector;
     int386(DPMI_INT, &regs, &regs);
 
-    if (regs.x.cflag)
+    if (regs.x.cflag != 0)
         return DPMI_ERROR;
 
     return DPMI_SUCCESS;

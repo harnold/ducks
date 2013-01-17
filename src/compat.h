68ae48be9f8cce2055d5cc320dcb74f57f7bb0ce
Holger Arnold <git@harnold.org>
2013-01-17 15:21:00 +0100

    compat: Add hword/lword functions, include stdint.h

--- src/compat.h
+++ src/compat.h
@@ -1,7 +1,12 @@
 #ifndef COMPAT_H
 #define COMPAT_H
 
+#include <stdint.h>
+
 #define array_length(a)         (sizeof(a) / sizeof((a)[0]))
+
+static inline uint32_t hword(uint32_t x) { return x >> 16; }
+static inline uint32_t lword(uint32_t x) { return x & 0xFFFF; }
 
 #if defined(__WATCOMC__)
 

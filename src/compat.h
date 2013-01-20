486cd2d2451651c1e29dd91e2d824f1fadbeb967
Holger Arnold <git@harnold.org>
2013-01-20 22:29:37 +0100

    compat: Add static_assert macro

--- src/compat.h
+++ src/compat.h
@@ -12,6 +12,15 @@ static inline uint32_t lword(uint32_t x) { return x & 0xFFFF; }
 
 #define PACKED_STRUCT   _Packed struct
 
+#ifndef _Static_assert
+#define _Static_assert(cond, msg) \
+    ((void) sizeof(struct { int __static_assert_failure: (cond) ? 1 : -1; }))
+#endif
+
+#ifndef static_assert
+#define static_assert _Static_assert
+#endif
+
 #else
 #error Unsupported compiler
 #endif

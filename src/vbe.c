64d1458e90cc556b47fa6609c22a80d6037b3be8
Holger Arnold <git@harnold.org>
2013-01-20 22:50:18 +0100

    vbe: Add function for obtaining mode-specific information

--- src/vbe.c
+++ src/vbe.c
@@ -23,6 +23,43 @@ PACKED_STRUCT vbe_info_block {
     uint8_t oem_data[256];
 };
 
+PACKED_STRUCT vbe_mode_info_block {
+    uint16_t mode_attributes;
+    uint8_t win_a_attributes;
+    uint8_t win_b_attributes;
+    uint16_t win_granularity;
+    uint16_t win_size;
+    uint16_t win_a_segment;
+    uint16_t win_b_segment;
+    uint32_t win_func_ptr;
+    uint16_t bytes_per_scanline;
+    uint16_t x_resolution;
+    uint16_t y_resolution;
+    uint8_t x_char_size;
+    uint8_t y_char_size;
+    uint8_t number_of_planes;
+    uint8_t bits_per_pixel;
+    uint8_t number_of_banks;
+    uint8_t memory_model;
+    uint8_t bank_size;
+    uint8_t number_of_image_pages;
+    uint8_t reserved_1;
+    uint8_t red_mask_size;
+    uint8_t red_field_position;
+    uint8_t green_mask_size;
+    uint8_t green_field_position;
+    uint8_t blue_mask_size;
+    uint8_t blue_field_position;
+    uint8_t rsvd_mask_size;
+    uint8_t rsvd_field_position;
+    uint8_t direct_color_mode_info;
+    uint32_t phys_base_ptr;
+    uint32_t reserved_2;
+    uint16_t reserved_3;
+    uint8_t reserved_4[17];
+    uint8_t reserved_5[189];
+};
+
 int vbe_get_info(struct vbe_info *info)
 {
     struct dpmi_rm_info rmi;
@@ -86,4 +123,47 @@ void vbe_destroy_info(struct vbe_info *info)
     free(info->oem_vendor_name);
     free(info->oem_product_name);
     free(info->oem_product_rev);
+}
+
+int vbe_get_mode_info(unsigned int mode, struct vbe_mode_info *info)
+{
+    struct dpmi_rm_info rmi;
+    uint16_t rm_seg, rm_sel;
+    struct vbe_mode_info_block *ib;
+
+    if (dpmi_allocate_dos_memory(sizeof(*ib), &rm_seg, &rm_sel) != 0)
+        goto failure;
+
+    ib = dpmi_ptr_to_rm_segment(rm_seg);
+    memset(ib, 0, sizeof(*ib));
+
+    memset(&rmi, 0, sizeof(rmi));
+    rmi.eax = 0x4F01;
+    rmi.ecx = mode;
+    rmi.es = rm_seg;
+
+    if (dpmi_simulate_rm_interrupt(VBE_INT, &rmi) != 0) {
+        dpmi_free_dos_memory(rm_sel);
+        goto failure;
+    }
+
+    if ((rmi.eax & 0xFF00) != 0) {
+        dpmi_free_dos_memory(rm_sel);
+        goto failure;
+    }
+
+    static_assert(offsetof(struct vbe_mode_info, phys_base_ptr) ==
+                  offsetof(struct vbe_mode_info_block, phys_base_ptr),
+                  "Wrong alignment of structure members");
+
+    memcpy(info, ib, sizeof(*info));
+
+    if (dpmi_free_dos_memory(rm_sel))
+        goto failure;
+
+    return 0;
+
+failure:
+
+    return error(0, "Obtaining VBE mode information failed");
 }
--- src/vbe.h
+++ src/vbe.h
@@ -12,6 +12,38 @@ enum vbe_capabilities {
     VBE_CAP_STEREO_SYNC_VIA_EVC_CONNECTOR       = 1 << 4
 };
 
+enum vbe_mode_attributes {
+    VBE_MA_MODE_SUPPORTED_BY_HARDWARE           = 1 << 0,
+    VBE_MA_BIOS_TTY_OUTPUT_FUNCTIONS_SUPPORTED  = 1 << 2,
+    VBE_MA_COLOR_MODE                           = 1 << 3,
+    VBE_MA_GRAPHICS_MODE                        = 1 << 4,
+    VBE_MA_MODE_NOT_VGA_COMPATIBLE              = 1 << 5,
+    VBE_MA_WINDOWED_FRAME_BUFFER_NOT_SUPPORTED  = 1 << 6,
+    VBE_MA_LINEAR_FRAME_BUFFER_SUPPORTED        = 1 << 7,
+    VBE_MA_DOUBLE_SCAN_MODE_SUPPORTED           = 1 << 8,
+    VBE_MA_INTERLACED_MODE_SUPPORTED            = 1 << 9,
+    VBE_MA_TRIPLE_BUFFERING_SUPPORTED           = 1 << 10,
+    VBE_MA_STEREO_DISPLAY_SUPPORTED             = 1 << 11,
+    VBE_MA_DUAL_DISPLAY_START_ADDRESS_SUPPORTED = 1 << 12
+};
+
+enum vbe_win_attributes {
+    VBE_WA_WINDOW_RELOCATABLE           = 1 << 0,
+    VBE_WA_WINDOW_READABLE              = 1 << 1,
+    VBE_WA_WINDOW_WRITABLE              = 1 << 2,
+};
+
+enum vbe_memory_model {
+    VBE_MM_TEXT_MODE                    = 0x00,
+    VBE_MM_CGA_GRAPHICS                 = 0x01,
+    VBE_MM_HERCULES_GRAPHICS            = 0x02,
+    VBE_MM_PLANAR                       = 0x03,
+    VBE_MM_PACKED_PIXEL                 = 0x04,
+    VBE_MM_NON_CHAIN_4_256_COLORS       = 0x05,
+    VBE_MM_DIRECT_COLOR                 = 0x06,
+    VBE_MM_YUV                          = 0x07
+};
+
 struct vbe_info {
     char vbe_signature[4];
     uint16_t vbe_version;
@@ -25,7 +57,41 @@ struct vbe_info {
     char *oem_product_rev;
 };
 
+struct vbe_mode_info {
+    uint16_t mode_attributes;
+    uint8_t win_a_attributes;
+    uint8_t win_b_attributes;
+    uint16_t win_granularity;
+    uint16_t win_size;
+    uint16_t win_a_segment;
+    uint16_t win_b_segment;
+    uint32_t win_func_ptr;
+    uint16_t bytes_per_scanline;
+    uint16_t x_resolution;
+    uint16_t y_resolution;
+    uint8_t x_char_size;
+    uint8_t y_char_size;
+    uint8_t number_of_planes;
+    uint8_t bits_per_pixel;
+    uint8_t number_of_banks;
+    uint8_t memory_model;
+    uint8_t bank_size;
+    uint8_t number_of_image_pages;
+    uint8_t reserved_1;
+    uint8_t red_mask_size;
+    uint8_t red_field_position;
+    uint8_t green_mask_size;
+    uint8_t green_field_position;
+    uint8_t blue_mask_size;
+    uint8_t blue_field_position;
+    uint8_t rsvd_mask_size;
+    uint8_t rsvd_field_position;
+    uint8_t direct_color_mode_info;
+    uint32_t phys_base_ptr;
+};
+
 int vbe_get_info(struct vbe_info *info);
 void vbe_destroy_info(struct vbe_info *info);
+int vbe_get_mode_info(unsigned int mode, struct vbe_mode_info *mode_info);
 
 #endif

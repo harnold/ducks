a54086e44f3608454adbbcecce5fdcd1ce5a92cb
Holger Arnold <git@harnold.org>
2013-01-28 13:12:02 +0100

    dpmi: Delete comments

--- src/dpmi.h
+++ src/dpmi.h
@@ -16,26 +16,13 @@ PACKED_STRUCT dpmi_rm_info {
     uint16_t es, ds, fs, gs, ip, cs, sp, ss;
 };
 
-/* Extended memory management */
-
 int dpmi_map_physical_address(uint32_t phys_addr, uint32_t size, uint32_t *lin_addr);
 int dpmi_unmap_physical_address(uint32_t lin_addr);
-
-/* DOS memory management */
-
 int dpmi_allocate_dos_memory(uint32_t size, uint16_t *segment, uint16_t *selector);
 int dpmi_free_dos_memory(uint16_t selector);
-
-/* Translation */
-
 int dpmi_simulate_rm_interrupt(unsigned int inum, struct dpmi_rm_info *info);
-
-/* Page management */
-
 int dpmi_lock_linear_region(uint32_t lin_addr, uint32_t size);
 int dpmi_unlock_linear_region(uint32_t lin_addr, uint32_t size);
-
-/* Other */
 
 static inline void *dpmi_ptr_to_rm_segment(uint16_t rm_seg)
 {

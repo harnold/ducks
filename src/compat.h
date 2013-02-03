4eeae228ae82cf91324cf1932ca2b32f26f04e81
Holger Arnold <git@harnold.org>
2013-02-03 16:57:52 +0100

    compat: Add stpcpy() function
    This function is defined in POSIX.1-2008.

--- src/compat.h
+++ src/compat.h
@@ -8,6 +8,14 @@
 static inline uint32_t hword(uint32_t x) { return x >> 16; }
 static inline uint32_t lword(uint32_t x) { return x & 0xFFFF; }
 
+static inline char *stpcpy(char *dst, const char *src)
+{
+    while (*src != '\0')
+        *dst++ = *src++;
+    *dst = *src;
+    return dst;
+}
+
 #if defined(__WATCOMC__)
 
 #define PACKED_STRUCT   _Packed struct

893bfd84739fc1052d8832eaa3509de7ca0fecf8
Holger Arnold <holgerar@gmail.com>
2014-01-25 23:25:57 +0100

    Add sprite.c, sprite.h

---
+++ src/sprite.c
@@ -0,0 +1,65 @@
+#include "sprite.h"
+
+#include <stdlib.h>
+
+DEFINE_ALLOCATOR(sprite, struct sprite, ALLOC_DEFAULT_BLOB_SIZE);
+
+void init_sprite_class(struct sprite_class *class, struct image *image,
+                      int width, int height, int num_frames)
+{
+    class->image = image;
+    class->width = width;
+    class->height = height;
+    class->num_frames = num_frames;
+}
+
+void init_sprite(struct sprite *sprite, const struct sprite_class *class,
+                 float x, float y, float v_x, float v_y, int z, int frame,
+                 const struct animation *anim)
+{
+    *((struct sprite_class *) sprite) = *class;
+    sprite->x = x;
+    sprite->y = y;
+    sprite->v_x = v_x;
+    sprite->v_y = v_y;
+    sprite->z = z;
+    sprite->frame = frame;
+    sprite->anim = anim;
+    sprite->anim_start_time = 0.0f;
+}
+
+void sprite_update(struct sprite *sprite, float t, float dt)
+{
+    sprite->x += dt * sprite->v_x;
+    sprite->y += dt * sprite->v_y;
+
+    const struct animation *anim = sprite->anim;
+
+    if (anim->type == ANIM_NONE)
+        return;
+
+    int nframe = (int) ((t - sprite->anim_start_time) * anim->fps);
+
+    switch (anim->type) {
+
+    case ANIM_ROTATE_FORWARD:
+        sprite->frame = anim->start_frame +
+            nframe % (anim->end_frame - anim->start_frame + 1);
+        break;
+
+    case ANIM_ROTATE_BACKWARD:
+        sprite->frame = anim->start_frame -
+            nframe % (anim->start_frame - anim->end_frame + 1);
+        break;
+
+    case ANIM_ONCE_FORWARD:
+        if (sprite->frame < anim->end_frame)
+            sprite->frame = min(anim->start_frame + nframe, anim->end_frame);
+        break;
+
+    case ANIM_ONCE_BACKWARD:
+        if (sprite->frame > anim->end_frame)
+            sprite->frame = max(anim->start_frame - nframe, anim->end_frame);
+        break;
+    }
+}
---
+++ src/sprite.h
@@ -0,0 +1,64 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include "alloc.h"
+#include "elist.h"
+
+#include <stdint.h>
+
+struct image;
+
+enum animation_type {
+    ANIM_NONE,
+    ANIM_ROTATE_FORWARD,
+    ANIM_ROTATE_BACKWARD,
+    ANIM_ONCE_FORWARD,
+    ANIM_ONCE_BACKWARD
+};
+
+struct animation {
+    enum animation_type type;
+    float fps;
+    int start_frame;
+    int end_frame;
+};
+
+struct sprite_class {
+    struct image *image;
+    int width;
+    int height;
+    int num_frames;
+};
+
+struct sprite {
+    struct image *image;
+    int width;
+    int height;
+    int num_frames;
+    float x;
+    float y;
+    float v_x;
+    float v_y;
+    int z;
+    int frame;
+    const struct animation *anim;
+    float anim_start_time;
+    struct elist_node link;
+};
+
+DECLARE_ALLOCATOR(sprite, struct sprite);
+
+#define sprite_list_for_each(__sprite, __list) \
+    elist_for_each_elem((__sprite), (__list), struct sprite, link)
+
+#define sprite_list_get(__node) \
+    elist_get(__node, struct sprite, link)
+
+void init_sprite_class(struct sprite_class *class, struct image *image,
+                       int width, int height, int num_frames);
+void init_sprite(struct sprite *sprite, const struct sprite_class *class,
+                 float x, float y, float v_x, float v_y, int z, int frame,
+                 const struct animation *anim);
+void sprite_update(struct sprite *sprite, float t, float dt);
+
+#endif

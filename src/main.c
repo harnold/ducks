05086227c14c80c91b4977177ec8a216db63ebb2
Holger Arnold <holgerar@gmail.com>
2014-01-25 23:50:32 +0100

    main: Add timer initialization and cleanup

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include "error.h"
+#include "timer.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GAME_TIMER_HZ   120.0f
+
 static void cleanup(void)
 {
+    timer_exit();
 }
 
 int main(void)
@@ -11,6 +16,11 @@ int main(void)
     error_set_log_file(stdout);
 
     atexit(cleanup);
+
+    if (timer_init(GAME_TIMER_HZ) != 0) {
+        error("Initializing timer failed");
+        goto failure;
+    }
 
     cleanup();
 

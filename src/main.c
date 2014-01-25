ebcc2f8e51f4073aceb5a53c6c0204dad1132566
Holger Arnold <holgerar@gmail.com>
2014-01-25 23:49:08 +0100

    main: Add cleanup() function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,26 @@
+#include "error.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+static void cleanup(void)
+{
+}
+
 int main(void)
 {
-    printf("DUCKS!\nCopyright 2000, 2012 Holger Arnold.\n");
+    error_set_log_file(stdout);
 
+    atexit(cleanup);
+
+    cleanup();
+
+    log("Everything alright.\n");
     exit(EXIT_SUCCESS);
+
+failure:
+
+    cleanup();
+
+    log("Something went wrong.\n");
+    exit(EXIT_FAILURE);
 }

09699a96ebea2498b7b86bf169ba4146ac31a3d3
Holger Arnold <holgerar@gmail.com>
2014-02-04 20:49:59 +0100

    xmalloc: Add xmalloc() / xfree() statistics

--- src/xmalloc.c
+++ src/xmalloc.c
@@ -4,6 +4,8 @@
 #include <errno.h>
 #include <stdlib.h>
 
+static struct xmalloc_stats xmalloc_stats;
+
 void *xmalloc(size_t size)
 {
     if (size == 0)
@@ -14,5 +16,19 @@ void *xmalloc(size_t size)
     if (!ptr)
         fatal_errno("Out of memory?  malloc() failed to allocate %ul bytes", size);
 
+    xmalloc_stats.xmalloc_calls++;
+    xmalloc_stats.allocated_bytes += size;
+
     return ptr;
+}
+
+void xfree(void *ptr)
+{
+    xmalloc_stats.xfree_calls++;
+    free(ptr);
+}
+
+void xmalloc_get_stats(struct xmalloc_stats *stats)
+{
+    *stats = xmalloc_stats;
 }
--- src/xmalloc.h
+++ src/xmalloc.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+struct xmalloc_stats {
+    int xmalloc_calls;
+    int xfree_calls;
+    size_t allocated_bytes;
+};
+
 void *xmalloc(size_t size);
+void xfree(void *ptr);
+void xmalloc_get_stats(struct xmalloc_stats *stat);
 
 #endif
